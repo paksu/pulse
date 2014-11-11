56396b8e4cf6c7ed3de5d6e35d41a3fc4f2b428d
joona <joona@giosg.com>
2014-11-11 23:56:40 +0200

    fixing layout

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,11 +2,8 @@
 
 angular.module('pulseApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.countries = [];
+    $scope.beacons = 0;
     socket.socket.on('msg', function (item) {
-      $scope.countries.unshift(item[0]);
-      if($scope.countries.length > 20) {
-        $scope.countries.pop();
-      }
+      $scope.beacons = $scope.beacons + 1;
     });
   });
